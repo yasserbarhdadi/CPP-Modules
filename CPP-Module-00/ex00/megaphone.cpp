28d0556366302a5dcade3ebd83fcfe69f40a977c
Yasser Barhdadi <yasser.barhdadi@hotmail.com>
2025-08-20 13:44:58 +0100

    ex00 done

---
+++ CPP-Module-00/ex00/megaphone.cpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yabarhda <yabarhda@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/20 13:06:38 by yabarhda          #+#    #+#             */
+/*   Updated: 2025/08/20 13:30:25 by yabarhda         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <cctype>
+
+int main(int ac, char **av)
+{
+	if (ac == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+	else
+	{
+		for (int i = 1; av[i]; i++)
+		{
+			for (int j = 0; av[i][j]; j++)
+			{
+				if (isalpha(av[i][j]) && islower(av[i][j]))
+					std::cout << (char)toupper(av[i][j]);
+				else
+					std::cout << av[i][j];
+			}
+		}
+		std::cout << "\n";
+	}
+	return (0);
+}
