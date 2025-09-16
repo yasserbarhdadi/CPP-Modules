afb563f41f48247836f8b3efaaaf52b1f698c330
Yasser Barhdadi <yasser.barhdadi@hotmail.com>
2025-09-16 10:50:08 +0100

    changed to switch statement

--- CPP-Module-01/ex06/Harl.cpp
+++ CPP-Module-01/ex06/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: yabarhda <yabarhda@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/16 10:24:51 by yabarhda          #+#    #+#             */
-/*   Updated: 2025/09/16 10:35:14 by yabarhda         ###   ########.fr       */
+/*   Updated: 2025/09/16 10:47:08 by yabarhda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,24 @@ void Harl::debug(void)
 {
 	std::cout << "[ DEBUG ]" << std::endl;
 	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!\n" << std::endl;
+	info();
+	warning();
+	error();
 }
 
 void Harl::info(void)
 {
 	std::cout << "[ INFO ]" << std::endl;
 	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!\n" << std::endl;
+	warning();
+	error();
 }
 
 void Harl::warning(void)
 {
 	std::cout << "[ WARNING ]" << std::endl;
 	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years, whereas you started working here just last month.\n" << std::endl;
+	error();
 }
 
 void Harl::error(void)
@@ -38,21 +44,27 @@ void Harl::error(void)
 
 void Harl::complain(std::string level)
 {
+	int index = 1337;
 	const std::string levels[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
-	void (Harl::*complaint[4])(void) = {
-		&Harl::debug,
-		&Harl::info,
-		&Harl::warning,
-		&Harl::error
-	};
 	for (int i = 0; i < 4; i++)
 	{
-		if (level == levels[i])
+		if (levels[i] == level)
 		{
-			for (int j = i; j < 4; j++)
-				(this->*complaint[j])();
-			return;
+			index = i;
+			break;
 		}
 	}
-	std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
+	switch (index)
+	{
+		case 0: debug();
+			break;
+		case 1: info();
+			break;
+		case 2: warning();
+			break;
+		case 3: error();
+			break;
+		default:
+			std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
+	}
 }
